4a19954f2b78c3501269e1d4c9a4607e6655ab99
RumKam <rumakansk@list.ru>
2025-01-26 20:31:12 +0000

    recsys

---
+++ recsys/config.py
@@ -0,0 +1,8 @@
+config = {
+    "RECS_URL": "http://127.0.0.1:8000",
+    "FEATURE_STORE_URL": "http://127.0.0.1:8010",
+    "EVENTS_STORE_URL": "http://127.0.0.1:8020",
+    "DEFAULT_PATH": "../recsys/recommendations/top_popular.parquet",
+    "PERSONAL_RECS_PATH": "../recsys/recommendations/recommendations.parquet",
+    "SIMILAR_RECS_PATH": "../recsys/recommendations/similar_items.parquet"
+     }
